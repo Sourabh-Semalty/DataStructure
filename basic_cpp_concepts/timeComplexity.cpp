1a0432c0ce633fddb5abba23b0a90266134f8436
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2021-12-15 20:30:26 +0530

    Learn 1. Time and space complexity 2. Data structure 3. types of data structure 4. Stack and heap memory

---
+++ basic_cpp_concepts/timeComplexity.cpp
@@ -0,0 +1,22 @@
+// Learned about space and timecomplexity for future algo purpose
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    // here time complexity is
+    // * i = 0 => 1 ; i < n => n ; i++ => n   
+    // O(n+1) => O(n)
+    for (int i = 0; i < n; i++)
+    {
+        cout << i; // n
+    }
+    return 0;
+}
+
+
+// Timecomplexity can be defined using the iteration or time consuming by any part of the program
+// Same goes for the Space complexity also
