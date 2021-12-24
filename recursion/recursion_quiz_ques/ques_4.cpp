c68b6c679fe51e74e4d90ddab2da5bfc26ad8419
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2021-12-24 22:54:26 +0530

    unsolved problem

--- recursion/recursion_quiz_ques/ques_4.cpp
+++ recursion/recursion_quiz_ques/ques_4.cpp
@@ -1,18 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void count(int n)
+class Solution
 {
-    static int d = 1;
-    cout << n;
-    cout << d;
-    d++;
-    if (n > 1) count(n - 1);
-    cout << d;
-}
+public:
+    double myPow(double x, int n)
+    {
+        if (n == 0)
+            return 1;
+        if (x < -100.0 || x > 100.0)
+            return 0;
+        if (x < 1 && x > 0)
+            return 0;
+        if (n <= INT_MIN || n >= INT_MAX)
+            return 1;
+        if (n > 0)
+            return myPow(x, n - 1) * x;
+        else
+            return myPow(x, n + 1) / x;
+    }
+};
 
 int main()
 {
-    count(3);
-    return 0;
+    Solution s;
+    cout << s.myPow(0.00001, 2147483647);
 }
