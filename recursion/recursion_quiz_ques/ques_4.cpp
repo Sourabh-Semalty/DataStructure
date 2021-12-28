43afdcc9792235c056b12b543b2658382db429f8
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2021-12-28 06:12:37 +0530

    Question 4 added with gitignore

--- recursion/recursion_quiz_ques/ques_4.cpp
+++ recursion/recursion_quiz_ques/ques_4.cpp
@@ -6,14 +6,14 @@ class Solution
 public:
     double myPow(double x, int n)
     {
-        if (n == 0)
-            return 1;
-        if (x < -100.0 || x > 100.0)
-            return 0;
-        if (x < 1 && x > 0)
-            return 0;
         if (n <= INT_MIN || n >= INT_MAX)
             return 1;
+        else if (n == 0)
+            return 1;
+        else if (x < -100.0 || x > 100.0)
+            return 0;
+        else if (x < 1 && x > 0)
+            return 0;
         if (n > 0)
             return myPow(x, n - 1) * x;
         else
