7f162b9ffd5cc7b271a9f005032fcc2748703edf
Sourabh Semalty <42134981+Sourabh-Semalty@users.noreply.github.com>
2021-12-28 06:09:21 +0530

    Learn 1. Array 2. Insert delete 3. Insert from index delete from index 4. done with class concept of c plus plus

--- Array/operation_with_array.cpp
+++ Array/operation_with_array.cpp
@@ -12,6 +12,10 @@ public:
     Arrayop(int size, int length);
     void insertElements();
     void displayElements();
+    void freeMemory();
+    int getLength();
+    void insertAtindex(int index, int value);
+    void deletefromindex(int index);
 };
 
 Arrayop::Arrayop(int size, int length)
@@ -24,15 +28,61 @@ Arrayop::Arrayop(int size, int length)
 void Arrayop::insertElements()
 {
     cout << "Enter all the elements" << endl;
-    for (int i = 0; i < this->size; i++)
+    for (int i = 0; i < this->size - 2; i++)
+    {
         cin >> A[i];
+        this->length += 1;
+    }
 }
 
 void Arrayop::displayElements()
 {
     cout << "Entered elements are :" << endl;
-    for (int i = 0; i < size; i++)
-        cout << A[i];
+    for (int i = 0; i < length; i++)
+        cout << A[i] << "\t";
+    cout << endl;
+}
+
+void Arrayop::freeMemory() { delete[] A; }
+
+int Arrayop::getLength() { return length; }
+
+void Arrayop::insertAtindex(int index, int val)
+{
+    // check if array is full
+    if (index > length)
+        cout << endl
+             << "Index is not valid";
+    else if (index == size)
+        cout << endl
+             << "Array is full";
+    else
+    {
+        for (int i = this->length; i >= index; i--)
+        {
+            A[i + 1] = A[i];
+        }
+        A[index] = val;
+        this->length++;
+    }
+}
+
+void Arrayop::deletefromindex(int index)
+{
+    if (index < 0)
+        cout << endl
+             << "index must be greater than -1" << endl;
+    else if (index > length)
+        cout << endl
+             << "Nothing in this index" << endl;
+    else
+    {
+        for (int i = index; i < length; i++)
+        {
+            A[i] = A[i + 1];
+        }
+        this->length--;
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -44,5 +94,15 @@ int main(int argc, char const *argv[])
 
     op.insertElements();
     op.displayElements();
+    cout << endl
+         << "The length of an array is " << op.getLength() << endl;
+
+    op.insertAtindex(3, 45);
+    op.displayElements();
+
+    op.deletefromindex(0);
+    op.displayElements();
+
+    op.freeMemory();
     return 0;
 }
